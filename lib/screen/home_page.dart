d19369f9e71ff05a48e7de0732cd1d5ed7773fd2
HyunWoo <iu4148@gmail.com>
2023-07-08 03:37:08 +0900

    tft app

--- lib/screen/home_page.dart
+++ lib/screen/home_page.dart
@@ -14,6 +14,7 @@ class homePage extends StatefulWidget {
 class _homePageState extends State<homePage> {
   var f = NumberFormat('###,###,###,###');
   String file_loc = 'assets/api_key.txt';
+  String? keys;
   List<String> dropList = [
     'KR', 'JP', 'NA', 'BR', 'LAN', 'LAS', 'EUNE', 'EUW', 'TR', 'RU', 'OCE'];
   String selectedValue = 'KR';
--- lib/screen/loading_page.dart
+++ lib/screen/loading_page.dart
@@ -16,51 +16,61 @@ class LoadingPage extends StatefulWidget {
 }
 
 class _LoadingPage extends State<LoadingPage> {
+  String file_loc = 'assets/api_key.txt';
+  String keys = '99';
   List<String> user_name = [];
   List<int> user_idx = [];
   List<int> user_points = [];
   List<int> temp_points = [];
+  void _loadData() async {
+    final _loadedData = await rootBundle.loadString('assets/api_key.txt');
+    setState(() {
+      keys = _loadedData;
+    });
+  }
   var url = Uri.parse('https://kr.api.riotgames.com/tft/league/v1/challenger');
-  Map<String, String> header = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36 Edg/114.0.1823.67",
-    "Accept-Language": "ko,en;q=0.9,en-US;q=0.8",
-    "Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8",
-    "Origin": "https://developer.riotgames.com",
-    "***",
-  };
-
-  Future<void> _API() async {
-    http.Response response = await http.get(url, headers: header);
-    var user_list = jsonDecode(response.body)['entries'];
-    if (user_points.length < 220) {
-      for (int i=0; i<user_list.length; i++) {
-        bool _isCheck = false;
-        if (i==0) {
-          user_name.add(user_list[i]['summonerName']);
-          user_points.add(user_list[i]['leaguePoints']);
-        }
-        else {
-          for (int j=0; j<i; j++) {
-            if (user_points[j] <= user_list[i]['leaguePoints']) {
-              user_name.insert(j, user_list[i]['summonerName']);
-              user_points.insert(j, user_list[i]['leaguePoints']);
-              _isCheck = true;
-              break;
-            }
-          }
-          if (_isCheck == false) {
-            user_name.add(user_list[i]['summonerName']);
-            user_points.add(user_list[i]['leaguePoints']);
-          }
-        }
-        print(i);
-      }
-    }
-    //var reverse = List.from(temp_points.reversed);
-    //print(temp_points);
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _loadData();
   }
   @override
   Widget build(BuildContext context) {
+    Map<String, String> header = {
+      "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36 Edg/114.0.1823.67",
+      "Accept-Language": "ko,en;q=0.9,en-US;q=0.8",
+      "Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8",
+      "Origin": "https://developer.riotgames.com",
+      "X-Riot-Token": keys,
+    };
+    Future<void> _API() async {
+      http.Response response = await http.get(url, headers: header);
+      var user_list = jsonDecode(response.body)['entries'];
+      if (user_points.length < 220) {
+        for (int i=0; i<user_list.length; i++) {
+          bool _isCheck = false;
+          if (i==0) {
+            user_name.add(user_list[i]['summonerName']);
+            user_points.add(user_list[i]['leaguePoints']);
+          }
+          else {
+            for (int j=0; j<i; j++) {
+              if (user_points[j] <= user_list[i]['leaguePoints']) {
+                user_name.insert(j, user_list[i]['summonerName']);
+                user_points.insert(j, user_list[i]['leaguePoints']);
+                _isCheck = true;
+                break;
+              }
+            }
+            if (_isCheck == false) {
+              user_name.add(user_list[i]['summonerName']);
+              user_points.add(user_list[i]['leaguePoints']);
+            }
+          }
+        }
+      }
+    }
     _API().then((value) => {
       Navigator.pushAndRemoveUntil(context, MaterialPageRoute (builder: (BuildContext context) => homePage(
         name_list: user_name, idx_list: user_idx, point_list: user_points,
