641212de5d6fc853586aacc7a0c635a8b85ed4ae
HyunWoo <iu4148@gmail.com>
2023-07-08 03:12:30 +0900

    tft app

--- lib/screen/loading_page.dart
+++ lib/screen/loading_page.dart
@@ -26,7 +26,7 @@ class _LoadingPage extends State<LoadingPage> {
     "Accept-Language": "ko,en;q=0.9,en-US;q=0.8",
     "Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8",
     "Origin": "https://developer.riotgames.com",
-    "X-Riot-Token": "RGAPI-f01ee8a6-2842-4707-b458-265973e7126a",
+    "***",
   };
 
   Future<void> _API() async {
