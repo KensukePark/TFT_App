e36d0294a3d557aae89b63870f5fb4999b7316a2
HyunWoo <iu4148@gmail.com>
2023-07-10 03:32:03 +0900

    tft app

--- lib/screen/community_page.dart
+++ lib/screen/community_page.dart
@@ -35,6 +35,7 @@ class _commuPageState extends State<commuPage> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
+        resizeToAvoidBottomInset : false,
         body: WillPopScope(
           onWillPop: onWillPop,
           child: Container(
